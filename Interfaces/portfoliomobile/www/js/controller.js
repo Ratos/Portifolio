b613c59903e19dca810e5e93c90f1a944f75c4e5
Thercio <thercio01395@hotmail.com>
2015-10-14 23:45:17 -0300

    thats it
    Módulos adicionados e controller de validação de campo adicionado

---
+++ Interfaces/portfoliomobile/www/js/controller.js
@@ -0,0 +1,41 @@
+/*
+	Aqui ficará o controller da aplicação
+*/
+(function() {
+  
+  'use strict';
+
+  angular
+    .module( 'formApp' )
+    .controller( 'FormController', FormController );
+
+  FormController.$inject = [ '$scope' ];
+
+  function FormController( $scope ) {
+
+    var vm = this;
+
+    ///////////// Properties
+    vm.customer = {};
+
+    ///////////// Public Methods
+    vm.save = save;
+
+    ///////////// Private Methods
+
+    /////////////
+    function save( data ) {
+
+      console.log( 'Data were successfully sent to the server!' );
+      console.log( JSON.stringify( data, null, 2 ));
+      
+      vm.customer = {};
+
+      // we need $scope to use this method
+      $scope.cadastroForm.$setPristine();
+
+    }
+
+  }
+
+}());
