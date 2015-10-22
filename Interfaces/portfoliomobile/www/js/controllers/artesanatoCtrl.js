e92ec3dec746ed7b799ada6c4d128d5e06d1693e
Darlan Oliveira <feitt.c6@gmail.com>
2015-10-21 22:02:46 -0200

    Adicionado funções de Cadastro de Artesanata
    as Função adicionadas estão sendo implementadas estão não é sua versão
    final

--- Interfaces/portfoliomobile/www/js/controllers/artesanatoCtrl.js
+++ Interfaces/portfoliomobile/www/js/controllers/artesanatoCtrl.js
@@ -1,7 +1,20 @@
-angular.module("app").controller('artesanatoCtrl', function($scope,$http,$rootScope,localStorageService){ 
+angular.module("app").controller('artesanatoCtrl', function($scope,$http,$rootScope,localStorageService,$location){ 
     
-    $scope.carregardados = function(){
-    	$scope.artesanatos = localStorageService.get('artesanatos');
-    	console.log($scope.artesanatos.nome);
-    };
+	$scope.artesao = localStorageService.get('artesao');
+	$scope.artesanato = {};
+
+   $scope.addArtesanato = function(artesanato){
+   	
+    $scope.artesanato = artesanato;
+   	$scope.artesanato.idArtesao = $scope.artesao.idArtesao;
+
+	   	$http.post("http://192.168.1.5:8080/Portifolio/artesanato/salvar", $scope.artesanato).success(function(data,status){
+	            console.log("Artesanato adicionado com sucesso")            
+	            $location.path('/initialpage');
+	    }).error(function(data,status){
+
+	    	console.error("Erro ao Cadastra Artesanato",data,status);
+	    }); 
+	   
+   }
 });
--- Interfaces/portfoliomobile/www/js/controllers/cadastroCtrl.js
+++ Interfaces/portfoliomobile/www/js/controllers/cadastroCtrl.js
@@ -1,10 +1,18 @@
-angular.module("app").controller('cadastroCtrl', function($scope,$http,$rootScope,localStorageService){ 
+angular.module("app").controller('cadastroCtrl', function($scope,$http,$rootScope){ 
     
     $scope.addArtesao = function(artesao){
-      $http.post("http://192.168.1.4:8080/portifolio/cadastro/save", artesao);
-    };
+      
+      $http.post("http://192.168.1.5:8080/Portifolio/cadastro/save", artesao).success(function(data, status){
+      		console.log("Cadastro Realizado com Sucesso ",data, status)
+      		$location.path('/login');
+      }).error(function(data, status){
 
-	$scope.addArtesanato = function(artesanatos) {
-		$http.post("http://192.168.1.4:8080/portifolio/artesanato/salvar", artesanatos);
-	};
+      		console.error("erro ao realizar o Cadastro ",status,data)	
+      });
+ 
+    }
+
+
+
+	
 });
